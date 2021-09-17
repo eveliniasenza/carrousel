5810260873326e4b5417d849936bc5dc6c7ed8f7
evelin <evelin.iasenza@silicon-access.com>
2021-09-17 18:58:26 -0300

    CAROUSEL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";  
+import Carousel from 'react-bootstrap/Carousel' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  return(  
+    <Carousel>
+    <Carousel.Item interval={800}>
+      <img
+       src="https://mdbootstrap.com/img/new/slides/043.jpg"
+       class="d-block w-100"
+       alt="..."
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={800}>
+      <img
+       src="https://mdbootstrap.com/img/new/slides/042.jpg"
+       class="d-block w-100"
+       alt="..."
+      />
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item interval={800}>
+      <img
+       src="https://mdbootstrap.com/img/new/slides/041.jpg"
+       class="d-block w-100"
+       alt="..."
+      />
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+  )
 }
-
-export default App;
+export default App
